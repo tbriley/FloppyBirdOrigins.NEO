ac8073a0ed0546a5a004ad70a28fe96e87fe1cc3
Tim Riley <tim@finergames.com>
2018-08-10 02:11:49 -0400

    - initial commit, starting building out first contract

---
+++ FloppyBirdOrigins.NEO/CharacterContract.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Numerics;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+
+namespace FloppyBirdOrigins.NEO
+{
+    public class CharacterContract : SmartContract
+    {
+        public static byte[] Main(string operation, object[] args)
+        {
+            var characterID = (string)args[0];
+            if (operation.Equals("get"))
+            {
+                byte[] data = Storage.Get(Storage.CurrentContext, characterID);
+                Runtime.Notify("CHARACTER_" + characterID, data.AsString());
+                return data;
+            }
+            else if(operation.Equals("transfer"))
+            {
+                return new BigInteger(0).AsByteArray();
+            }
+            return new BigInteger(0).AsByteArray();
+        }
+    }
+}
